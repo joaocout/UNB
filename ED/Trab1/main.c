1e88bfd9097336cb0d1d428fa6e8634b2c4820dd
jpedro <jpedro0801@gmail.com>
2018-10-09 12:39:54 -0300

    09/10

--- ED/Trab1/main.c
+++ ED/Trab1/main.c
@@ -1,41 +1,9 @@
 #include <stdio.h>
 #include "pilha.h"
 
+
+/*uso para teste de funcoes*/
 int main () {
-	tipo_pilha* pilha = aloca_pilha();
 	
-	empilha(pilha, "127938");
-	empilha(pilha, "1ASdas");
-	empilha(pilha, "asdas");
-	empilha(pilha, "Asdfa8");
-	empilha(pilha, "1wsdas8");
-	printf("qtd = %d | topo = %s\n", pilha->quantidade, pilha->topo->dados);
-	print_pilha(pilha);
-	printf("\n");
-
-	desempilha(pilha);
-	desempilha(pilha);
-	printf("qtd = %d | topo = %s\n", pilha->quantidade, pilha->topo->dados);
-	print_pilha(pilha);
-	printf("\n");
-
-	desempilha(pilha);
-	desempilha(pilha);
-	printf("qtd = %d | topo = %s\n", pilha->quantidade, pilha->topo->dados);
-	print_pilha(pilha);
-	printf("\n");
-
-	desempilha(pilha);
-	printf("qtd = %d |\n", pilha->quantidade);
-	printf("\n");
-
-	empilha(pilha, "a");
-	empilha(pilha, "olasldjh");
-	printf("qtd = %d | topo = %s\n", pilha->quantidade, pilha->topo->dados);
-	print_pilha(pilha);
-	printf("\n");	
-
-
-	remove_pilha(pilha);
 	return 0;
 }
