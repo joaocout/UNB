9d806d153058613608accacb57f172079a5f2d87
joaocout <jpedro0801@gmail.com>
2018-10-12 10:40:11 -0300

    update

--- ED/Trab1/floattostring.c
+++ ED/Trab1/floattostring.c
@@ -1,29 +1,71 @@
 #include <stdio.h>
 #include <string.h>
 
-void floattostring (float num, char* aux){
+void floattostring (double num, char* string){
     int negativo=0;
     if(num<0) {
         negativo=1;
-        aux[0] = '-';
+        string[0] = '-';
         num = -num;
     }
+    int posvirgula=negativo, comvirgula=0;
     int inum = num, i = negativo;
-    if(inum==num){/*sem casa fracionaria*/
+
+
+    if(inum==num){/*sem parte fracionaria*/
         while(num>=1){
-            aux[i] = (inum%10) + '0';
+            string[i] = (inum%10) + '0';
             num = num/10;
             inum = num;
             i++;    
         }
     }
+
+    else{/*com parte   fracionaria*/
+        comvirgula = 1;
+        double temp = num;
+        while(temp>=1){
+            temp = temp/10;
+            posvirgula++;
+        }
+        while(inum!=num){
+            num=num*10;
+            inum=num;
+        }
+            printf("num sem virgula: %f\n", num);
+        while(num>=1){
+            string[i] = (inum%10) + '0';
+            num = num/10;
+            inum = num;
+            i++;    
+        }
+    }
+
     /*inverter string*/
-    char temp[strlen(aux)];
-    for(i=negativo; i<strlen(aux); i++)
-        temp[i-negativo] = aux[strlen(aux)-1-i+negativo];
-    
-    for(i=negativo; i<strlen(aux); i++)
-        aux[i] = temp[i-negativo];
+    char temp[strlen(string)];
+    if(!negativo){
+        for(i=0; i<strlen(string); i++){
+            
+        }
+    }
+    for(i=negativo; i<strlen(string); i++)
+        temp[i-negativo] = string[strlen(string)-1-i+negativo];
+    printf("%s\n", temp);
+    if(!comvirgula){
+        for(i=negativo; i<strlen(string); i++)
+            string[i] = temp[i-negativo];    
+    }
+    else{
+        int j=0;
+        for(i=negativo; i<strlen(temp); i++){
+            if(j==posvirgula){
+                string[j] = ',';
+                j++;
+            }
+            string[j] = temp[i-negativo];
+            j++;
+        }
+    }
 }
 
 int main () {
