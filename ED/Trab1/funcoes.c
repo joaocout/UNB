3a1ebe41a4e802e01c9eb05d6dead2d85ad58a56
joaocout <jpedro0801@gmail.com>
2018-10-15 10:26:10 -0300

    funcoes de conversao finalizadas

---
+++ ED/Trab1/funcoes.c
@@ -0,0 +1,100 @@
+#include "funcoes.h"
+#include <math.h>
+#include <string.h>
+
+double stringtodouble(char* string){
+	int comvirgula=0, posvirgula, negativo=0;
+	if(string[0]=='-'){
+		negativo = 1;
+		string[0] = '0';	
+	}
+	for(int i=0; i<strlen(string); i++){
+		if(string[i]=='.' || string[i]==','){
+			comvirgula=1;
+			posvirgula=i;
+		}
+	}
+    double a = 0;
+	if(!comvirgula){
+		for(int i=0; i<strlen(string); i++)
+			a += (string[i]-'0') * pow(10, strlen(string)-i-1);		
+	}
+	else{
+		for(int i=0; i<posvirgula; i++){
+			double temp = string[i] - '0';
+			a += temp * pow(10, posvirgula-1-i);
+		}
+		for(int i= posvirgula+1; i<strlen(string); i++){
+			double temp = string[i] - '0';
+			a += temp * pow(10, posvirgula-i);
+		}
+	}
+	if(negativo) return -a;
+	else return a;
+}
+
+void inverter(char* string){
+    for(int i=0, j=strlen(string)-1; i<j; i++, j--){
+        char aux = string[i];
+        string[i] = string[j];
+        string[j] = aux;
+    }
+}
+void doubletostring (double num, char* string){
+    
+    int negativo = 0;
+    if(num<0){
+        negativo=1;
+        num = -num;
+    }
+
+    int inum = num;
+    if(num==inum){ /*numero inteiro*/
+        int i = 0;
+        do{/*num gravado ao contrario*/
+            string[i] = (inum%10) + '0';
+            inum=inum/10;
+            i++;
+        }while(inum>=1);
+        if(negativo) string[strlen(string)] = '-';
+        inverter(string);
+    }
+
+    else{ /*numero quebrado*/
+        int i=0;
+        int posvirgula=negativo;
+        do{/*descobrir posicao da virgula;*/
+            inum=inum/10;
+            posvirgula++;
+        }while(inum>=1);
+
+        int entrezeroeum = 0;
+        if(num>0 && num<1) entrezeroeum=1;
+        
+        inum = num;
+        while(num-inum>=0.00001){
+            num=num*10;
+            inum=num;
+        }
+        i=0;
+        do{/*string gravada sem virgula*/
+            string[i] = (inum%10) + '0';
+            inum=inum/10;
+            i++;
+        }while(inum>=1);
+        if(entrezeroeum) string[strlen(string)] = '0';
+        if(negativo) string[strlen(string)] = '-';
+        inverter(string);
+        char aux = string[posvirgula];
+        for(i=posvirgula; i<strlen(string)+1; i++){
+            if(i==posvirgula){
+                string[i] = ',';
+            }
+            else{
+                char temp = string[i];
+                string[i] = aux;
+                aux = temp;
+            }
+        }
+    }
+}
---
+++ ED/Trab1/funcoes.h
@@ -0,0 +1,15 @@
+#ifndef _FUNCOES_H_
+#define _FUNCOES_H_
+
+/*funcoes utilizadas no trabalho*/
+
+
+/*recebe uma string e a transforma em um numero ponto flutante de precisao dupla*/
+double stringtodouble(char* string);
+
+
+/*recebe um numero em ponto flutuante e uma string onde sera salva
+a transformacao desse numero para string*/
+void doubletostring(double num, char* string);
+
+#endif
