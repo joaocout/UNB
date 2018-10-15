0d27e825bfe6bed3ca479be9e925778f2d808456
joaocout <jpedro0801@gmail.com>
2018-10-15 16:42:05 -0300

    update

--- ED/Trab1/trab.c
+++ ED/Trab1/trab.c
@@ -2,13 +2,17 @@
 #include "funcoes.h"
 #include "pilha.h"
 
+
 int main (){
     tipo_pilha* pilha = aloca_pilha();
-        empilha(pilha, "12,98");
-        empilha(pilha, "63,15");
-        empilha(pilha, "90,12");
-        empilha(pilha, "2234");
-        print_pilha(pilha);
-        remove_pilha(pilha);
+    empilha(pilha, "12.6565");
+    empilha(pilha, "11231.6565");
+    empilha(pilha, "12.6123565");
+    empilha(pilha, "12abd5");
+    empilha(pilha, "10985");
+    empilha(pilha, "1Asd565");
+    empilha(pilha, "112365");
+    print_pilha(pilha);
+    remove_pilha(pilha);
     return 0;
 }
