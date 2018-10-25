7704db319fc05a87d24bc9e806a085c77b4debe9
joaocout <jpedro0801@gmail.com>
2018-10-25 14:16:57 -0300

    update

--- PC/Lista3_2/c.cpp
+++ PC/Lista3_2/c.cpp
@@ -1,25 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int visited[250000];
+
+vector <vector <int>> graph(250000);
+
+void bfs(int start){
+    queue<int> q;
+    q.push(start);
+    visited[start] = 1;
+
+    while(q.size()){
+        int u = q.front();
+        q.pop();
+        for(int w: graph[u]){
+            if(!visited[w]){    
+                q.push(w);
+                visited[w] = 1;
+            }
+        }
+    }
+}
+
 int main () {
     int n, m;
     cin >> n >> m;
-    string tab[n];
+    
+    char mapa[n][m];
 
-    for(int i=0; i<n; i++) cin >> mapa[i];
-
-    vector <vector <int>> graph(n*m);
-
+    //ler mapa
+    for(int i=0; i<n; i++) scanf("%s", mapa[i]);
+    
+    //montar grafo
     for(int i=0; i<n; i++) for(int j=0; j<m; j++){
-        if(mapa[i][j]=='.'){
-            mapa[(i*m)+j].push_back((i*m)+j);
-            if(i!=0 && tab[i-1][j]!='#') mapa[(i*m)+j].push_back([((i-1)*m)+j]);
-            if(i!=n-1 && tab[i+1][j]!='#') mapa[(i*m)+j].push_back([((i+1)*m)+j]);
-            if(j!=0 && tab[i][j-1]!='#') mapa[(i*m)+j].push_back([(i*m)+j-1]);
-            if(j!=m-1 && tab[i][j+1]!='#') mapa[(i*m)+j].push_back([(i*m)+j+1]);
+        
+        if(mapa[i][j]=='#') visited[(i*m)+j] = 1;
+        
+        else if(mapa[i][j]=='.'){
+            
+            //graph[(i*m)+j].push_back((i*m)+j);
+            
+            if(i!=0 && mapa[i-1][j]!='#') graph[(i*m)+j].push_back(((i-1)*m)+j);
+            
+            if(i!=n-1 && mapa[i+1][j]!='#') graph[(i*m)+j].push_back(((i+1)*m)+j);
+            
+            if(j!=0 && mapa[i][j-1]!='#') graph[(i*m)+j].push_back((i*m)+j-1);
+            
+            if(j!=m-1 && mapa[i][j+1]!='#') graph[(i*m)+j].push_back((i*m)+j+1);
         }
     }
-    
+
+    int r=0;
+    for(int i=0; i<n*m; i++){
+        if(visited[i]==0){
+            bfs(i);
+            r++;
+        }
+    }
+        
+    printf("%d\n", r);
 
     return 0;
 }
