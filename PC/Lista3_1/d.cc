53292cd918ee5be4e1699d246883f336dc5a1e1d
joaocout <jpedro0801@gmail.com>
2018-10-27 22:52:16 -0300

    a

---
+++ PC/Lista3_1/d.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    long long n, k;
+    cin >> n >> k;
+    long long tab[n];
+    for(int i=0; i<n; i++) scanf("%d", &tab[i]);
+
+    int soma=0;
+    for(int i=0; i<n; i++) soma += tab[i];
+    
+    int possivel = 1;
+    if(soma<k) possivel=0;
+    45jm
+    
+
+}
