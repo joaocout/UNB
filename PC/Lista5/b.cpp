35d3c3dbdfec470eda031bf4c59592e22efb554a
João Coutinho <39359575+joaocout@users.noreply.github.com>
2018-11-28 17:21:32 -0200

    problema da mochila booleana

--- PC/Lista5/b.cpp
+++ PC/Lista5/b.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tab[1000];
-pair<int, int> itens[1000];
-int n;
-
-int min(int a, int b){
-    if(a<=b) return a;
-    else return b;
-}
-
-int dp(int w){
-    if(w==0) return 0;
-    if(tab[i]) return tab[i];
-    
-}
+int tab[1001][1001];
 
 int main () {
-    int w;
-    cin >> w >> n;
-    for(int i=0; i<n; i++)
-        scanf("%d", &itens[i].first);
-    for(int i=0; i<n; i++)
-        scanf("%d", &itens[i].second);
 
-    cout << dp(w) << endl;
+    int w, n;
+    cin >> w >> n;
+    
+    int peso[n];
+    int valor[n];
+
+    for(int i=0; i<n; i++) scanf("%d", &peso[i]);
+    for(int i=0; i<n; i++) scanf("%d", &valor[i]);
+
+    for(int i=1; i<=n; i++){
+
+        for(int j=1; j<=w; j++){
+
+            if(peso[i-1]<=j){
+                tab[i][j] = max(tab[i-1][j], tab[i-1][j-peso[i-1]]+valor[i-1]);
+            }
+
+            else tab[i][j] = tab[i-1][j];
+        }
+    }
+
+    cout << tab[n][w] << endl;
+
     return 0;
 }
