d6c9bfe0ac83811b456c85ade3e7c4ab04d3f9c7
joaocout <joaopedro0801@hotmail.com>
2019-08-25 12:33:56 -0300

    leitura de arquivo e criação do grafo feitos corretamente

---
+++ TAG/Projeto1/proj1.cpp
@@ -0,0 +1,82 @@
+/*
+ALUNO : JOAO PEDRO ASSUNCAO COUTINHO
+MATRICULA : 18/0019813
+github.com/joaocout
+*/
+
+/*
+O programa pula todos os comentarios (linhas iniciadas com '%')
+independentemente da linha em que estao escritos.
+*/
+
+/*
+O primeiro input nao comentado deve ser composto por tres inteiros
+responsaveis por descrever quantas arestas e vertices o grafo possui
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+
+    string file_name;
+    cout << "Enter the  input file name: ";
+    cin >> file_name;
+
+    ifstream input_file;
+    input_file.open(file_name);
+
+    if(input_file.fail()){
+        cout << "Error opening the file / file doesn't exist" << endl;
+    }
+    else{
+
+        string line;
+        bool first_valid_input = false;
+        int height, width, edges;
+
+        //primeiro encontramos o primeiro input, para declarar o grafo corretamente
+        while(!first_valid_input && getline(input_file, line)){
+
+            if(line[0] != '%'){             //se a linha n for um comentario
+
+                if(first_valid_input == false){ //se o primeiro input (que nao seja um comentario) nao tenha sido lido ainda
+                    first_valid_input = true;
+                    stringstream stream(line);   //transformando a string 'line' em um stream
+                    stream >> height >> width >> edges;
+                }
+
+            }
+
+            line.clear();
+
+        }
+
+        vector<vector<int>> dolphins(height + 1);
+
+        //depois lemos as conexoes
+        while(getline(input_file, line)){   //ate o fim do arquivo
+            int a, b;
+            stringstream stream(line);
+            stream >> a >> b;
+            cout << a << " " << b << endl;
+            
+            //o grafo nao eh direcionado, entao ha conexao nas duas direcoes
+            dolphins[a].push_back(b);
+            dolphins[b].push_back(a);
+
+            line.clear();
+
+        }
+
+        input_file.close();
+
+    } 
+
+    return 0;
+}
